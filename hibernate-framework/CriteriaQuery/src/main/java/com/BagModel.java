5d84f97111f4362b715ed884ba9d714fd1258261
bharat-kadchha <bharatkadchha1@gmail.com>
2020-06-17 15:43:54 +0530

    third commit

---
+++ hibernate-framework/CriteriaQuery/src/main/java/com/BagModel.java
@@ -0,0 +1,27 @@
+package com;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.ToString;
+
+
+@Entity
+@Table(name="student_bag")
+@Data
+@ToString
+public class BagModel {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	
+	int prize;
+	
+	
+}
